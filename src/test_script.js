9db5c616c6632acba25982f15633101f56823e13
lucasdev <blesslucas89@gmail.com>
2024-03-14 09:40:32 -0500

    script teste para criar 100 transações diferentes, E int

--- src/test_script.js
+++ src/test_script.js
@@ -1,35 +1,45 @@
 const axios = require('axios');
 
-const apiURL = 'http://localhost:3000/transactions';// aqui vc coloca a url da sua api
+//função para gerar um ID unico para cada transação
+function generateUniqueId() {
+    return Math.random().toString(36).substring(2, 0)
+}
 
-async function createTransaction(id) {
-    const transaction = {
-        idempotencyId: 'idempotency_${id}',
-        amount: Math.floor(Math.random() * 1000) + 1, //valor aleatorio entre 1 e 10
-        type: 'purchase',
-        creditDebit: 'debit'
-    };
+//função para gerar um tipo de transação aleatório (credit debit)
+function generateTransactionType() {
+    const types = [' credit', 'debit'];
+    return types[Math.floor(Math.random() * types.length)];
+}
 
-
-    try{
-        await axios.post(apiURL, transaction);
-        console.log('Transação ${id} criada com sucesso');
-    }catch (error) {
-        if(error.response && error.response.data){
-            console.error('Error ao criar transação ${id}', error.response.data);
-        } else {
-            console.error('Erro ao criar transação ${id}', error.massage);
-        }
-
-    }
-
-async function createMultipleTransactions(numTransactions){
-    for (let i = 1; i <= numTransactions; i++) {
-        await createTransaction(i);
+//função para gerar uma transação de valores aleatorios
+function genarateTransiction() {
+    return {
+        idempotencyId: generateUniqueId(),
+        amount:Math.floor(Math.random() * 1000), //valor aleatorio de 0 a 1000
+        type: generateTransictionType(),
+        creditDebit :Math.random() < 0.5 ? 'credit' : 'debit' // escolhe aleatoriamente entre credito e debito
     }
 }
 
-const numTransactions = 100;
-
-createMultipleTransactions(numTransactions);
+//função para enviar transação para a API
+async function sendTransaction(transaction) {
+    try {
+        const response = await axios.post('http://localhost:3000/transictions', transaction);
+        console.log('Transação enviada com sucesso', response.data);
+    } catch (error) {
+        console.error('erro ao enviar transações', error.massage);
+    }
 }
+
+//criar e enviar 100 transações diferentes
+async function sendMultipleTransactions() {
+    const transactions = Array.from({ langth : 100}, () => generateTransactionType());
+
+    for (const transaction of transactions) {
+        await sendTransaction(transaction);
+    }
+}
+
+//Executar a função para enviar as transações
+
+sendMultipleTransactions();
